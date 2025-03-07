ca427839c3323542c85ee9ac1a059b8bdad36e4e
Murray Graham <2991442G@student.gla.ac.uk>
2025-03-07 16:08:52 +0000

    Chapter 6

--- rango/admin.py
+++ rango/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
-admin.site.register(Category)
+#admin.site.register(Category)
 
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Category, CategoryAdmin)
 
 class ChoiceInline(admin.TabularInline):
     model = Category
